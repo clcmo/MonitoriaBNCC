e17597684adb5ab51df3514cc3da96163792f6d2
Camila Leite (Milla) <ola@camilaloliveira.com>
2026-09-22 21:09:45 -0300

    Fix error

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import 'bulma/css/bulma.min.css'
-import '/assets/css/style.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
